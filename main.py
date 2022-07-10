08b6e156dcdf6bb99333e32bdaac7d195285587b
The-Briel-Deal <gabe@fordltc.net>
2022-07-10 14:38:11 -0400

    Wrote up and commented out solution using hashsets

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+from collections import defaultdict
+
+
+# At a high level, we can think of the problem as follows:
+# A board is valid if there are no duplicates in any row, column, or 3x3 box.
+# Because of this we are going to iterate through every space, cache the values in hashsets as we go,
+# and check if the current value is in any of the 3 corresponding hashsets.
+# If it is, we return false.
+# If we iterate through every space and don't find any duplicates, we return true.
 def valid_sudoku(board):
+    # Using default dicts as our data structure so that I don't have to explicitly create keys for each row, column, and box.
+    cols = defaultdict(set)
+    rows = defaultdict(set)
+    squares = defaultdict(set)
+    # Iterate through the board.
     for i in range(9):
         for j in range(9):
-            print(i, j)
+            # If the current cell is empty, continue.
+            if board[i][j] == ".":
+                continue
+            num = int(board[i][j])
+            # Check if the number is already in the row, column, or square. If so, return False.
+            if num in rows[i] or num in cols[j] or num in squares[(i // 3, j // 3)]:
+                return False
+            # Finally at the end of every iteration we add this number to the row, column, and square so we know if we see a duplicate.
+            cols[j].add(num)
+            rows[i].add(num)
+            squares[(i // 3, j // 3)].add(num)
     return True
 
 
